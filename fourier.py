505833c54aedba889b911a37e8b1cb6a894e41ea
molor824 <molormargaderdene@gmail.com>
2025-04-19 22:04:10 +0800

    fix: use numpy features more and make the integration much faster

--- fourier.py
+++ fourier.py
@@ -5,7 +5,7 @@ import numpy as np
 import cmath
 import math
 
-DT = 0.001
+DT = 0.0001
 
 def time_range(a: float, b: float, dt: float):
     t = a
@@ -21,23 +21,26 @@ def integrate_c(n: int, points: list[complex], timeframes: list[float]):
     b = timeframes[-1]
     interval = b - a
 
-    velocity = -math.tau * n / interval
+    sample_count = round(interval / DT)
+    theta = -1j * math.tau * n / interval
+    time_samples = np.linspace(a, b, sample_count)
 
-    def inner(t: float):
-        point = np.interp(t, timeframes, points)
-        return point * cmath.rect(1, velocity * t)
-    return integ.trapezoid([inner(t) for t in time_range(a, b, DT)], dx=DT) / interval
+    return integ.simpson(
+        np.interp(time_samples, timeframes, points) * np.exp((time_samples - a) * theta),
+        time_samples,
+    ) / interval
 
 def pool_integrate_c(args):
     return integrate_c(*args)
 
 class FourierSeries:
-    def __init__(self, max_n: int, coefficients: np.ndarray[tuple, np.dtype[complex]]):
+    def __init__(self, max_n: int, coefficients: np.ndarray[tuple, np.dtype[complex]], interval: float):
         if max_n * 2 + 1 > coefficients.shape[0]:
             raise ArgumentError(None, "coefficients does not match max_n size")
 
         self.coefficients = coefficients
         self.max_n = max_n
+        self.interval = interval
 
     def from_points(max_n: int, points: list[complex], timeframes: list[float]):
         if len(points) <= 1:
@@ -46,7 +49,7 @@ class FourierSeries:
             return FourierSeries(max_n, np.array(
                 pool.map(pool_integrate_c, ((i, points, timeframes) for i in range(-max_n, max_n + 1))),
                 dtype=complex
-            ))
+            ), timeframes[-1] - timeframes[0])
 
     def arrows(self, t: float):
-        return (self.coefficients[i + self.max_n] * cmath.rect(1.0, cmath.tau * i * t) for i in range(-self.max_n, self.max_n + 1))
+        return self.coefficients * np.exp(1j * cmath.tau / self.interval * t * np.arange(-self.max_n, self.max_n + 1))
